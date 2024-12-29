496b5be122abc2029e8dc623fac914c56a677459
leander <foolsparadise984@gmail.com>
2024-12-29 13:54:32 +0800

    removed border-b

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="w-full py-14">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="max-w-3xl mx-auto">
-                    <ul className="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500  mb-10 border-b border-gray-200">
+                    <ul className="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500  mb-10">
                         <li>
                             <a href="#" className="text-gray-800 hover:text-gray-900">Home</a>
                         </li>
